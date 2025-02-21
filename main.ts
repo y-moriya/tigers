cabac4c3814eb2de2d5687ed7f79e2ec698d0a52
Yu MORIYA <euro21st@gmail.com>
2025-02-21 20:04:08 +0900

    Deno cronジョブのタスク名を修正して、より明確な表現に変更

--- main.ts
+++ main.ts
@@ -216,7 +216,7 @@ async function main() {
   }
 }
 
-Deno.cron("Get live information for tigers.", "0 19 * * *", async () => {
+Deno.cron("Get today's live information for tigers", "0 19 * * *", async () => {
   console.info("Start crawling.");
   await main();
 });
